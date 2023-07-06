2a71d0c333b92f5d67a78b6ac1ee0e3f1f98310f
Skell <85579428+SkellXC@users.noreply.github.com>
2023-07-06 14:51:01 +0100

    Cleaned up sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -5,28 +5,11 @@ import static as fd
 import keyboard
 import pytesseract
 import pyautogui
-import cv2
-import re
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"#You need to replace this with the filepath to tesseract.exe
 myconfig = r"--psm 11 --oem 3"
-'''sleep(1)
-screenshot = pyautogui.screenshot()
-#image = t.getRound(screenshot)
-#image.show()
-#start = time.time()
-screenshot.show()
-text = pytesseract.image_to_string(screenshot,config=myconfig) 
-
-
-print(text)'''
-#screenshot = pyautogui.screenshot()
-#print(t.checkScreenFor("heroes"))
-
-#t.enterMap("darkCastle","HARD_MODE","STANDARD_GAME_MODE")
-targets = {#can only be used once realistically
-    "last":1,
-    "close":2,
-    "strong":3
-
-}
-print(targets["close"])
+"""
+https://muthu.co/all-tesseract-ocr-options/
+Use it to find the ones you feel are right but in general
+11 is fine for searching the whole screen and 7 is for 
+checking what round the game is on.
+"""
