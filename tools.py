0821896f217c36922b418162813214e27f868f86
Ethan Travasso <flameboyepic@gmail.com>
2026-09-08 11:52:32 +0100

    Removed tesseract related functions & added functions to handle the game end state

--- tools.py
+++ tools.py
@@ -3,85 +3,101 @@ import keyboard
 import numpy as np
 from time import sleep
 import win32api, win32con
-from PIL import Image
-import cv2
-import pytesseract
 import re
 import static as fd#fd = fixeddata
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-# myconfig = r"--psm 7 --oem 3"#pytesseract config
-csfi = r"--psm 11 --oem 3"#Config for checkScreenForItem
-def getPosition():
-    coords = []
-    print("Press 'n' to log the coordinates.\nPress 'c' to exit")
-    while not keyboard.is_pressed("c"):
-        
-        
-        if keyboard.is_pressed("n"):
-            x,y = pyautogui.position()
-            print(f"({x},{y})")
-            sleep(0.5)
-            coords.append((x,y))
-            print("Press 'n' to log another set of coordinates.\nPress 'c' to exit")
+from typing import Literal
 
-    for x in range(0,len(coords)):
-        print(f"Position {x+1}: {coords[x]}")
-    print("Exited program")
-
-
-#-------------------------------------------------------------------------------------------------    
 
 def click(x,y):
-    win32api.SetCursorPos((x,y))#Sets the cursor where it needs to be clicked
+    win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    sleep(0.2)#Delay needs to be there.
+    sleep(0.2)# Do not adjust
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-
-def checkScreenFor(item):
-    #Uses config #11 on pytesseract to search for any text (takes 2-3 seconds usually) and returns it
-    #It then looks for the item and returns true if it finds it or tries again in 15 seconds.
-    #Could cause issues. Change how it works or use carefully
-    sleep(1)
-    screenshot = pyautogui.screenshot()
-    #start = time.time()
-    text = pytesseract.image_to_string(screenshot,config=csfi) 
-    #end = time.time()
-    #print(f"It took {end-start} seconds to run this")
-    #print(text)
-    if item in text.lower():
-        #print("Item found!")
+def isOnHomeScreen() -> bool:
+    try:
+        pyautogui.locateOnScreen(r'images/home_play_button.PNG', confidence=0.8)
         return True
-
-    else:
-        print(f"checkScreenForItem has not found '{item}'")
-        sleep(5)
-
-    
-#------------------------------------------------------------------------------------------------- 
-
-def enterMap(map,difficulty,gamemode):
-    #(fd.buttons["homePlayButton"][0],"x")
-    click((fd.buttons["homePlayButton"][0],"x"),(fd.buttons["homePlayButton"][1],"y"))
-    sleep(1)
-    goToMapPage(12)
-    sleep(1)
-    click(fd.mapLocations[map][0],"x",fd.mapLocations[map][1],"y")
-    sleep(1)
-    click((fd.buttons[difficulty][0],"x"),(fd.buttons[difficulty][1],"y"))
-    sleep(1)
-    click((fd.buttons[gamemode][0],"x"),(fd.buttons[gamemode][1],"y"))
+    except:
+        return False
 
 
-def goToMapPage(page):
-    #sleep(0.1)
-    click((0.630078125,"x"),(0.8981481481481481,"y"))
+def enterMap(page: int, map: str, difficulty: str , gamemode: str):
+    click((fd.map_navigation_buttons["HOME_PLAY_BUTTON"][0]),
+          (fd.map_navigation_buttons["HOME_PLAY_BUTTON"][1]))
     sleep(1)
-    click((0.36640625,"x"),(0.8962962962962963,"y"))
+    _goToMapPage(page)
     sleep(1)
-    for x in range(0,page):
-        click((0.7640625,"x"),(0.39444444444444443,"y"))
+    click(fd.map_locations[map][0],
+          fd.map_locations[map][1])
+    sleep(1)
+    click((fd.choose_gamemode_buttons[difficulty][0]),
+          (fd.choose_gamemode_buttons[difficulty][1]))
+    sleep(1)
+    click((fd.choose_gamemode_buttons[gamemode][0]),
+          (fd.choose_gamemode_buttons[gamemode][1]))
+    print("Waiting for map to load.")
+    sleep(8)
+    print("Map has been loaded")
+    keyboard.send("enter")
+
+
+def _goToMapPage(page):
+    expert = fd.map_navigation_buttons["EXPERT_BUTTON"]
+    beginner = fd.map_navigation_buttons["BEGINNER_BUTTON"]
+    rightArrow = fd.map_navigation_buttons["RIGHT_ARROW"]
+    sleep(0.1)
+    click(expert[0], expert[1])
+    sleep(1)
+    click(beginner[0], beginner[1])
+    sleep(1)
+    for x in range(0,page-1):
+        click(rightArrow[0], rightArrow[1])
         sleep(0.1)
 
 
 
+def correctRound(correctRound: int, previousRound: int, intendedRound: int):
+    if correctRound == intendedRound:
+        if correctRound == previousRound:# cr = current round
+            return False# pr = previous round
+        else:
+            return True
+    # This is here because when the round starts, it places everything however since
+    # it checks at fixed intervals, it ends up redoing the same thing until the round
+    # changes. This function stops that. Idk if theres a better way to do it.
+
+def handleGameEnd():
+    try:
+        gameWon = pyautogui.locateOnScreen(r'images/next.PNG', confidence=0.8)
+        return 'win'
+
+    except pyautogui.ImageNotFoundException:
+        try:
+            homeButtonIsVisible = pyautogui.locateOnScreen(r'images/endHomeButton.PNG',
+                                                        confidence=0.8)
+            if homeButtonIsVisible:
+                return 'loss'
+        except pyautogui.ImageNotFoundException:
+            return None
+
+def _gameWonSequence():
+    click(955,909)# Click 'next'
+    sleep(3)
+    click(719, 849)# Click 'home'
+    sleep(5)
+
+def _gameLostSequence():
+    click(719, 849)# Click 'home' on the loss page
+    sleep(5)
+
+def endGame(state: Literal['win','loss']):
+    state = state.lower()
+
+    if state == 'win':
+        _gameWonSequence()
+
+    elif state == 'loss':
+        _gameLostSequence()
+    else:
+        return None
